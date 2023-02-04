2f5347af6a34cfc9b81782e94041962508d767c3
chrischriscris <18-10892@usb.ve>
2023-02-04 16:51:15 -0400

    added time measurements

--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,16 @@
  * Copyright (C) 2023 Christopher Gómez, Ka Fung
  */
 
+#include <functional>
 #include <iostream>
+#include <chrono>
 
 #include "../include/BruteForceSolver.hpp"
 #include "../include/SATInstance.hpp"
 
 using namespace std;
+
+double measure_time(function<void()> f);
 
 /**
  * @brief Initialize the solver
@@ -31,9 +35,35 @@ int main(int argc, char *argv[]) {
     // - See if we can factorize the weight computation into another function,
     // and maybe make use of the watchlist for that purpose
     SATInstance instance(argv[1]);
+
+    // Initialize every solver
     BruteForceSolver solver(instance);
-    solver.solve();
+
+    // Benchmark every solver
+    double brute_force_t = measure_time([&] { solver.solve(); });
+
+    // For debugging purposes
     solver.print_solution();
 
+    // Verify the solution is valid
+    // solver.verify_solution();
+
+    // Print the time elapsed
+    cout << "Time elapsed:" << endl;
+    cout << "  Exact solver: " << brute_force_t << "s" << endl;
+
     return 0;
+}
+
+/**
+ * @brief Function to measure the time elapsed executing a function
+ * 
+ * @param f The function to be executed
+ * @return double The time elapsed in seconds
+ */
+double measure_time(function<void()> f) {
+    auto start = chrono::high_resolution_clock::now();
+    f();
+    auto end = chrono::high_resolution_clock::now();
+    return chrono::duration_cast<chrono::duration<double>>(end - start).count();
 }
